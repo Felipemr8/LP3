949c73be844edc31de6d2d7bcdb520bc3825c36e
Felipemr8 <128558420+Felipemr8@users.noreply.github.com>
2023-04-19 18:42:45 -0300

    Delete Lista03/Dados directory

--- Lista03/Dados/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        Data d1 = new Data(2, 11, 2022);
-        Data d2 = new Data(2, 11, 2022);
-
-        System.out.println(d2.howManyDays(d1));
-        System.out.println(d2.dayOfWeek(d1));
-    }
-}
