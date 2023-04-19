b67b62a435fbc27706902640a108f6f5aec26be5
Felipemr8 <128558420+Felipemr8@users.noreply.github.com>
2023-04-19 18:31:02 -0300

    Create Main.java

---
+++ Lista03/Lampada/Main.java
@@ -0,0 +1,15 @@
+public  class  Principal {
+    public  static  void  principal ( String [] args ){
+        //Lampada l = new Lampada(12.4, "Azul", "Corporação", 5.4, 50, 100, 5);
+        Lampada  l = nova  Lampada ();
+
+        eu . setCor ( "Azul" );
+        eu . setManufacturer ( "Light Corporation" );
+        eu . setPotencyWatts ( 12.4 );
+        eu . definirQuantidadeEmEstoque ( 100 );
+        eu . valor definido ( 25 );
+        eu . setVoltage ( 5.4 );
+        eu . façaVenda ( 5 );
+        eu . imprimirCompra ();
+    }
+}
